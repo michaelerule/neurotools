88eea3e27f929a6083b7b9418b00ea6559d1a8cf
Michael <mrule@MacBook.local>
2017-07-06 12:23:56 +0100

    removes files specified in .gitignore

--- sync.sh
+++ sync.sh
@@ -12,8 +12,11 @@
 
 shopt -s extglob
 ./go
+# hopefully removes everything that shouldn't be being tracked as per
+# .gitignore. Possibly dangerous. 
+cat .gitignore | awk "/^[.\*]/" | sed 's/"/"\\""/g;s/.*/"&"/' |  xargs -E '' -I{} git rm -rf --cached {}
+git rm -rf --cached *.pyc
 git add . 
 git add -u :/
-git rm -rf *.pyc
 git commit -m "$1"
 git push origin master

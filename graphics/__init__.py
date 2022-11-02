b76314deca37fcc85e0559e6552985694d3fb227
M Rule <mrule7404@gmail.com>
2022-11-02 18:03:00 +0000

    rebuilt documentation

--- graphics/__init__.py
+++ graphics/__init__.py
@@ -8,12 +8,15 @@ from __future__ import generators
 from __future__ import unicode_literals
 from __future__ import print_function
 
-from . import color
 from . import colormaps
 from . import dendron
-from . import fabio_maps
 from . import image
 from . import matplotlib
 from . import plot
 from . import pygame
 from . import ztransform_plot_helpers
+
+
+# Don't import these, they will be pulled in by plot
+#from . import color
+#from . import fabio_maps

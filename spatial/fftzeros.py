05781a0402c79bff3b78aa1c1d6550a5d2c934c1
M Rule <mrule7404@gmail.com>
2022-11-02 17:43:08 +0000

    rebuilt documentation

--- spatial/fftzeros.py
+++ spatial/fftzeros.py
@@ -19,7 +19,7 @@ from neurotools.signal import rewrap
 from neurotools.graphics.plot import *
 from neurotools.spatial.triangulation import mergeNearby
 from neurotools.util.array import zeroslike,find
-from neurotools.util.array import c2p
+from neurotools.util.tools import c2p
 
 def plot_phase_gradient(dz,skip=1,lw=1,zorder=None,cmap='hsv'):
     '''

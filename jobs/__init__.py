f2aed729aec2de5d81819421b95a954e9c6226ce
M Rule <mrule7404@gmail.com>
2022-11-02 18:01:57 +0000

    rebuilt documentation

--- jobs/__init__.py
+++ jobs/__init__.py
@@ -8,11 +8,13 @@ from __future__ import generators
 from __future__ import unicode_literals
 from __future__ import print_function
 
-from . import cache
 from . import closure
 from . import filenames
 
 # These must be imported in a very careful sequence
+# by user scripts, since importing them triggers
+# a re-definition of the memoize decorator
+#from . import cache
 #from . import initialize_system_cache
 #from . import ndecorator
 #from . import parallel

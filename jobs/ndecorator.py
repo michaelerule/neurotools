c8199b7ad22a5d89f032d170750839f50a546fed
michaelerule <mrule7404@gmail.com>
2022-11-07 10:48:57 +0000

    rebuilt documentation

--- jobs/ndecorator.py
+++ jobs/ndecorator.py
@@ -236,8 +236,8 @@ def argument_signature(function,*args,**kwargs):
         for k in named[-ndefaulted:]:
             if k in named_store: raise ValueError(
                 'Duplicate argument',k)
-            named_store[k] = kwargs[k] 
-                if k in kwargs else default_map[k]
+            named_store[k] = kwargs[k] if k in kwargs 
+                else default_map[k]
             if k in kwargs: del kwargs[k]
             
     # Store excess positional arguments in *varargs
